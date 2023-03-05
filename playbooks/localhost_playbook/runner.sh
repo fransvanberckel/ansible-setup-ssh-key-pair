3c10ae5c837229e6fdb9d2def4261374460b75cb
Frans van Berckel <fberckel@xs4all.nl>
2023-03-05 17:09:26 +0100

    remove apt-get install ansible from localhost playbook

--- playbooks/localhost_playbook/runner.sh
+++ playbooks/localhost_playbook/runner.sh
@@ -3,35 +3,7 @@
 # set -x
 # set -e
 
-# GROUP_PLAYBOOK=$1
 GROUP_PLAYBOOK=localhost.yml
 PYTHONUNBUFFERED=1
 
-if ! command -v ansible >/dev/null; then
-
-        echo "Installing Ansible"
-
-        if command -v sudo >/dev/null; then
-
-                if command -v apt-get >/dev/null; then
-
-                        sudo apt-get update
-                        sudo DEBIAN_FRONTEND=noninteractive apt-get --yes install ansible
-
-                else
-                        echo "apt-get command not found!"
-                        exit 1
-
-                fi
-
-        else
-
-                echo "sudo command not found!"
-                exit 1
-
-        fi
-else
-
 ansible-playbook --verbose --ask-vault-pass $GROUP_PLAYBOOK
-
-fi
